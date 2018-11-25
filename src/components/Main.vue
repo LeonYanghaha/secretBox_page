8eeb68d726dad67123afe2e1fa6ba02553c43d80
Yanghaha <yangkai030208@163.com>
2018-11-25 12:45:19 +0800

    嘞😢

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="table_main">
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="appname" label="应用" width="180"></el-table-column>
-      <el-table-column prop="accountname" label="账户名" width="180"></el-table-column>
-      <el-table-column prop="password" label="密码"></el-table-column>
-      <el-table-column prop="desc" label="描述"></el-table-column>
-      <el-table-column prop="date" label="创建日期"></el-table-column>
-    </el-table>
+  <div>
+    <div class="add_area">
+    </div>
+    <div class="table_main">
+      <div class="table_control">
+        <span @click="addItem"><el-tag>添加新密码</el-tag></span>
+        <span><el-tag>批量删除</el-tag></span>
+      </div>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="appname" label="应用" width="180"></el-table-column>
+        <el-table-column prop="accountname" label="账户名" width="180"></el-table-column>
+        <el-table-column prop="password" label="密码"></el-table-column>
+        <el-table-column prop="desc" label="描述"></el-table-column>
+        <el-table-column prop="date" label="创建日期"></el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -16,11 +24,17 @@ export default {
   name: 'Main',
   data () {
     return {
-      tableData: []
+      tableData: [],
+      showAddArea:false
     }
   },
   methods: {
+    addItem () {
 
+      // this.$alert('以此填写相关信息，点击添加，就ok了...', '添加新的密码项.😆', {
+      //   confirmButtonText: '添加'
+      // })
+    }
   },
   mounted () {
     let url = conf.url + 'secret/getsecret'
@@ -40,17 +54,16 @@ export default {
         return
       }
       // 这里都是获取成功的情况，需要解析数据
-      let dataObj = data.data?data.data.secret?data.data.secret:null:null // JSON.parse(data.data)
+      let dataObj = data.data ? data.data.secret ? data.data.secret : null : null
       for (let i = 0; i < dataObj.length; i++) {
         let tempObj = dataObj[i]
         _self.tableData.push({
-          appname:tempObj['AppName']?tempObj['AppName']:'',
-          accountname:tempObj['AccountName']?tempObj['AccountName']:'',
-          password:tempObj['Password']?tempObj['Password']:'',
-          desc:tempObj['Describe']?tempObj['Describe']:'',
-          date:tempObj['CreateDate']?tempObj['CreateDate']:''
+          appname: tempObj['AppName'] ? tempObj['AppName'] : '',
+          accountname: tempObj['AccountName'] ? tempObj['AccountName'] : '',
+          password: tempObj['Password'] ? tempObj['Password'] : '',
+          desc: tempObj['Describe'] ? tempObj['Describe'] : '',
+          date: tempObj['CreateDate'] ? tempObj['CreateDate'] : ''
         })
-
       }
     })
   }
@@ -59,10 +72,25 @@ export default {
 
 <style scoped>
 .table_main{
-  border: 2px solid red;
+  /*border: 2px solid red;*/
   width: 70%;
   margin-top: 5%;
   margin-left: auto;
   margin-right: auto;
+}
+.table_control{
+  /*border: 2px solid green;*/
+  float: right;
+  margin-bottom: 3%;
+}
+.table_control span {
+  margin-left: 5px;
+}
+.add_area{
+  width: 100%;
+  height: 100%;
+  z-index: 99;
+  border: 2px solid green;
+  display: none;
 }
 </style>
